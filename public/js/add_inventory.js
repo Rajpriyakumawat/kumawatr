8f6fdb047d8183006bf6f47c3c2a0db24930fb16
Rajpriya Kumawat <kumawatr@classwork.engr.oregonstate.edu>
2024-05-30 12:04:02 -0700

    Update the add inventory to public folder

--- public/js/add_inventory.js
+++ public/js/add_inventory.js
@@ -1,53 +1,69 @@
-// Get the objects we need to modify
-let addInventoryForm = document.getElementById('add-inventory');
+//this module adapted from the starter code
+//osu-cs340 nodejs-starter-app
+//date 5/29/24
+//URL source: https://github.com/osu-cs340-ecampus/nodejs-starter-app
 
-// Modify the objects we need
-addInventoryForm.addEventListener("submit", function (e) {
-    
-    // Prevent the form from submitting
-    e.preventDefault();
+// Function to add inventory data
+function addInventoryData() {
+    // Get the objects we need to modify
+    let addInventoryForm = document.getElementById('add-inventory-form-ajax');
 
-    let inputProductType = document.getElementById("input-productType")
-    let inputQuantity = document.getElementById("input-quantity")
-    let inputLocation = document.getElementById("input-location")
+    // Modify the objects we need
+    addInventoryForm.addEventListener("submit", function (e) {
+        
+        // Prevent the form from submitting
+        e.preventDefault();
+        
+        // Get form fields we need to get data from
+        let inputProductType = document.getElementById("input-product-type");
+        let inputQuantity = document.getElementById("input-quantity");
+        let inputLocation = document.getElementById("input-location");
 
-    let productTypeValue = inputProductType.value;
-    let quantityValue = inputQuantity.value;
-    let locationValue = inputLocation.value;
+        // Get the values from the form fields
+        let productTypeValue = inputProductType.value;
+        let quantityValue = inputQuantity.value;
+        let locationValue = inputLocation.value;
+        
 
-    let data = {
-        productType: productTypeValue,
-        quantity: quantityValue,
-        location: locationValue
-    }
+        // Put our data we want to send in a JavaScript object
+        let data = {
+            productType: productTypeValue,
+            quantity: quantityValue,
+            location: locationValue
+        };
+        
+        // Setup our AJAX request
+        var xhttp = new XMLHttpRequest();
+        xhttp.open("POST", "/add-inventory-ajax", true);
+        xhttp.setRequestHeader("Content-type", "application/json");
 
-    // Setup our AJAX request
-    var xhttp = new XMLHttpRequest();
-    xhttp.open("POST", "/inventory", true);
-    xhttp.setRequestHeader("Content-type", "application/json");
+        // Tell our AJAX request how to resolve
+        xhttp.onreadystatechange = () => {
+            if (xhttp.readyState == 4 && xhttp.status == 200) {
 
-    // Tell our AJAX request how to resolve
-    xhttp.onreadystatechange = () => {
-        if (xhttp.readyState == 4 && xhttp.status == 200) {
-            // Add the new data to the table
-            addRowToTable(xhttp.response);
-            console.log('xhttp.response', xhttp.response);
+                // Add the new data to the table
+                addRowToTable(xhttp.response);
 
-            inputProductType.value = '';
-            inputQuantity.value = '';
-            inputLocation.value = '';
-
-        } else if (xhttp.readyState == 4 && xhttp.status != 200) {
-            console.log("There was an error with the input.")
+                // Clear the input fields for another transaction
+                inputProductType.value = '';
+                inputQuantity.value = '';
+                inputLocation.value = '';
+            }
+            else if (xhttp.readyState == 4 && xhttp.status != 200) {
+                console.log("There was an error with the input.")
+            }
         }
-    }
 
-    // Send the request and wait for the response
-    xhttp.send(JSON.stringify(data));
-});
+        // Send the request and wait for the response
+        xhttp.send(JSON.stringify(data));
 
-// Creates a single row from an Object representing a single record from 
-// inventory
+    })
+}
+
+// Call the function to add inventory data
+addInventoryData();
+
+// Creates a single row in the inventory table
 addRowToTable = (data) => {
     // Get a reference to the current table on the page and clear it out.
     let currentTable = document.getElementById("inventory-table");
@@ -57,66 +73,24 @@ addRowToTable = (data) => {
 
     // Get a reference to the new row from the database query (last object)
     let parsedData = JSON.parse(data);
-    console.log('parsed data', parsedData);
     let newRow = parsedData[parsedData.length - 1];
-    console.log('newRow', newRow);
 
-    // Create a row
-    let row = document.createElement("TR");
+    // Create a row and cells
+    let row = document.createElement("tr");
+    let productTypeCell = document.createElement("td");
+    let quantityCell = document.createElement("td");
+    let locationCell = document.createElement("td");
 
-    // iteratively add all the other data cells to the row
-    for (let key in newRow) {
-        let nextCell = document.createElement("TD");
-        nextCell.innerText = newRow[key];
-        row.appendChild(nextCell);
-    }
+    // Fill the cells with correct data
+    productTypeCell.innerText = newRow.productType;
+    quantityCell.innerText = newRow.quantity;
+    locationCell.innerText = newRow.location;
 
-    // Add the delete button to the row
-    let deleteCell = document.createElement("TD");
-    let deleteButton = document.createElement("button");
-    deleteButton.innerHTML = "Delete";
-    deleteButton.onclick = function() {
-        deleteInventory(newRow.inventoryID);
-    };
-    deleteCell.appendChild(deleteButton);
-    row.appendChild(deleteCell);
+    // Add the cells to the row
+    row.appendChild(productTypeCell);
+    row.appendChild(quantityCell);
+    row.appendChild(locationCell);
 
-    // Add a row attribute so the deleteRow function can find a newly added row
-    row.setAttribute('data-value', newRow.inventoryID);
-    
     // Add the row to the table
     currentTable.appendChild(row);
-}
-
-// Function to delete an inventory item
-deleteInventory = (id) => {
-    let data = { id: id };
-
-    // Setup our AJAX request
-    var xhttp = new XMLHttpRequest();
-    xhttp.open("DELETE", "/inventory", true);
-    xhttp.setRequestHeader("Content-type", "application/json");
-
-    // Tell our AJAX request how to resolve
-    xhttp.onreadystatechange = () => {
-        if (xhttp.readyState == 4 && xhttp.status == 204) {
-            deleteRow(id);
-        } else if (xhttp.readyState == 4 && xhttp.status != 204) {
-            console.log("There was an error with the deletion.")
-        }
-    }
-
-    // Send the request and wait for the response
-    xhttp.send(JSON.stringify(data));
-}
-
-// Deletes a row from the table
-deleteRow = (inventoryID) => {
-    let table = document.getElementById("inventory-table");
-    for (let i = 0, row; row = table.rows[i]; i++) {
-        if (table.rows[i].getAttribute("data-value") == inventoryID) {
-            table.deleteRow(i);
-            break;
-        }
-    }
 }
