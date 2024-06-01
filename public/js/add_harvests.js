f642e09e397be0fb27fbfbeb1897d2a8371ade20
Sirus Salari <salaris@classwork.engr.oregonstate.edu>
2024-05-31 22:17:07 -0700

    Create .hbs files for each entity and fix some code in app.js

---
+++ public/js/add_harvests.js
@@ -0,0 +1,94 @@
+// Get the objects we need to modify
+let addHarvestsForm = document.getElementById('add-harvests-form-ajax');
+
+// Modify the objects we need
+addHarvestsForm.addEventListener("submit", function (e) {
+
+    // Prevent the form from submitting
+    e.preventDefault();
+
+    // Get form fields we need to get data from
+    let inputFarmerID = document.getElementById("input-farmer-id");
+    let inputCropType = document.getElementById("input-crop-type");
+    let inputQuantity = document.getElementById("input-quantity");
+    let inputHarvestDate = document.getElementById("input-harvest-date");
+
+    // Get the values from the form fields
+    let farmerIDValue = inputFarmerID.value;
+    let cropTypeValue = inputCropType.value;
+    let quantityValue = inputQuantity.value;
+    let harvestDateValue = inputHarvestDate.value;
+
+    // Put our data we want to send in a javascript object
+    let data = {
+        farmerID: farmerIDValue,
+        cropType: cropTypeValue,
+        quantity: quantityValue,
+        harvestDate: harvestDateValue
+    }
+
+    // Setup our AJAX request
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("POST", "/add-harvests-ajax", true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+
+    // Tell our AJAX request how to resolve
+    xhttp.onreadystatechange = () => {
+        if (xhttp.readyState == 4 && xhttp.status == 200) {
+
+            // Add the new data to the table
+            addRowToHarvestsTable(xhttp.response);
+
+            // Clear the input fields for another transaction
+            inputFarmerID.value = '';
+            inputCropType.value = '';
+            inputQuantity.value = '';
+            inputHarvestDate.value = '';
+        }
+        else if (xhttp.readyState == 4 && xhttp.status != 200) {
+            console.log("There was an error with the input.")
+        }
+    }
+
+    // Send the request and wait for the response
+    xhttp.send(JSON.stringify(data));
+})
+
+// Creates a single row from an Object representing a single record from Harvests
+addRowToHarvestsTable = (data) => {
+
+    // Get a reference to the current table on the page and clear it out.
+    let currentTable = document.getElementById("harvests-table");
+
+    // Get the location where we should insert the new row (end of table)
+    let newRowIndex = currentTable.rows.length;
+
+    // Get a reference to the new row from the database query (last object)
+    let parsedData = JSON.parse(data);
+    let newRow = parsedData[parsedData.length - 1]
+
+    // Create a row and 5 cells
+    let row = document.createElement("TR");
+    let idCell = document.createElement("TD");
+    let farmerIDCell = document.createElement("TD");
+    let cropTypeCell = document.createElement("TD");
+    let quantityCell = document.createElement("TD");
+    let harvestDateCell = document.createElement("TD");
+
+    // Fill the cells with correct data
+    idCell.innerText = newRow.harvestID;
+    farmerIDCell.innerText = newRow.farmerID;
+    cropTypeCell.innerText = newRow.cropType;
+    quantityCell.innerText = newRow.quantity;
+    harvestDateCell.innerText = newRow.harvestDate;
+
+    // Add the cells to the row 
+    row.appendChild(idCell);
+    row.appendChild(farmerIDCell);
+    row.appendChild(cropTypeCell);
+    row.appendChild(quantityCell);
+    row.appendChild(harvestDateCell);
+
+    // Add the row to the table
+    currentTable.appendChild(row);
+}
