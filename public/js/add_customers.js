a769d3ffe723b457b9a5cfec4ca9a33a555e27ec
Rajpriya Kumawat <kumawatr@classwork.engr.oregonstate.edu>
2024-06-06 11:28:35 -0700

    updating the cistomers.js file for collacorating

--- public/js/add_customers.js
+++ public/js/add_customers.js
@@ -3,7 +3,6 @@ let addCustomersForm = document.getElementById('add-customers-form-ajax');
 
 // Modify the objects we need
 addCustomersForm.addEventListener("submit", function (e) {
-
     // Prevent the form from submitting
     e.preventDefault();
 
@@ -23,45 +22,35 @@ addCustomersForm.addEventListener("submit", function (e) {
         contactPerson: contactPersonValue,
         location: locationValue
     }
-
+    
     // Setup our AJAX request
-    var xhttp = new XMLHttpRequest();
-    xhttp.open("POST", "/add-customers-ajax", true);
+    let xhttp = new XMLHttpRequest();
+    xhttp.open("POST", "/customers", true);
     xhttp.setRequestHeader("Content-type", "application/json");
 
     // Tell our AJAX request how to resolve
     xhttp.onreadystatechange = () => {
         if (xhttp.readyState == 4 && xhttp.status == 200) {
-
             // Add the new data to the table
-            addRowToCustomersTable(xhttp.response);
+            addRowToCustomersTable(data);
 
             // Clear the input fields for another transaction
             inputName.value = '';
             inputContactPerson.value = '';
             inputLocation.value = '';
-        }
-        else if (xhttp.readyState == 4 && xhttp.status != 200) {
-            console.log("There was an error with the input.")
+        } else if (xhttp.readyState == 4 && xhttp.status != 200) {
+            console.log("There was an error with the input.");
         }
     }
 
     // Send the request and wait for the response
     xhttp.send(JSON.stringify(data));
-})
+});
 
 // Creates a single row from an Object representing a single record from Customers
 addRowToCustomersTable = (data) => {
-
     // Get a reference to the current table on the page and clear it out.
     let currentTable = document.getElementById("customers-table");
-
-    // Get the location where we should insert the new row (end of table)
-    let newRowIndex = currentTable.rows.length;
-
-    // Get a reference to the new row from the database query (last object)
-    let parsedData = JSON.parse(data);
-    let newRow = parsedData[parsedData.length - 1]
 
     // Create a row and 4 cells
     let row = document.createElement("TR");
@@ -71,17 +60,17 @@ addRowToCustomersTable = (data) => {
     let locationCell = document.createElement("TD");
 
     // Fill the cells with correct data
-    idCell.innerText = newRow.customerID;
-    nameCell.innerText = newRow.name;
-    contactPersonCell.innerText = newRow.contactPerson;
-    locationCell.innerText = newRow.location;
+    idCell.innerText = data.customerID;
+    nameCell.innerText = data.name;
+    contactPersonCell.innerText = data.contactPerson;
+    locationCell.innerText = data.location;
 
     // Add the cells to the row 
     row.appendChild(idCell);
     row.appendChild(nameCell);
     row.appendChild(contactPersonCell);
     row.appendChild(locationCell);
-
+    
     // Add the row to the table
     currentTable.appendChild(row);
 }
