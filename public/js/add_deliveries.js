c29227fee5d82374e12b9fa6f9a7fd8934c9d840
Sirus Salari <salaris@classwork.engr.oregonstate.edu>
2024-06-09 21:30:06 -0700

    Set up front end delivery form

---
+++ public/js/add_deliveries.js
@@ -0,0 +1,75 @@
+// Get the objects we need to modify
+let addDeliveryForm = document.getElementById('add-delivery-form-ajax');
+
+// Modify the objects we need
+addDeliveryForm.addEventListener("submit", function (e) {
+    // Prevent the form from submitting
+    e.preventDefault();
+
+    // Get form fields we need to get data from
+    let inputOrderID = document.getElementById("input-order-id");
+    let inputDeliveryDate = document.getElementById("input-delivery-date");
+
+    // Get the values from the form fields
+    let orderIDValue = inputOrderID.value;
+    let deliveryDateValue = inputDeliveryDate.value;
+
+    // Put our data we want to send in a javascript object
+    let data = {
+        orderID: orderIDValue,
+        deliveryDate: deliveryDateValue,
+    }
+    
+    // Setup our AJAX request
+    let xhttp = new XMLHttpRequest();
+    xhttp.open("POST", "/deliveries", true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+
+    // Tell our AJAX request how to resolve
+    xhttp.onreadystatechange = () => {
+        if (xhttp.readyState == 4 && xhttp.status == 200) {
+            // Add the new data to the table
+            addRowToDeliveriesTable(xhttp.response);
+
+            // Clear the input fields for another transaction
+            inputOrderID.value = '';
+            inputDeliveryDate.value = '';
+        } else if (xhttp.readyState == 4 && xhttp.status != 200) {
+            console.log("There was an error with the input.");
+        }
+    }
+
+    // Send the request and wait for the response
+    xhttp.send(JSON.stringify(data));
+});
+
+// Creates a single row from an Object representing a single record from Inventory
+addRowToDeliveriesTable = (data) => {
+    // Get a reference to the current table on the page and clear it out.
+    let currentTable = document.getElementById("deliveries-table");
+
+    let parsedData = JSON.parse(data)
+    let newRow = parsedData[parsedData.length - 1];
+
+    // Create a row and 5 cells
+    let row = document.createElement("TR");
+    let idCell = document.createElement("TD");
+    let orderIDCell = document.createElement("TD");
+    let deliveryDateCell = document.createElement("TD");
+
+    // Fill the cells with correct data
+    idCell.innerText = newRow.deliveryID;
+    orderIDCell.innerText = newRow.orderID;
+    deliveryDateCell.innerText = newRow.deliveryDate;
+
+    
+    // Add the cells to the row 
+    row.appendChild(idCell);
+    row.appendChild(orderIDCell);
+    row.appendChild(deliveryDateCell);
+
+    row.setAttribute('data-value', newRow.deliveryID);
+    
+    // Add the row to the table
+    currentTable.appendChild(row);
+}
