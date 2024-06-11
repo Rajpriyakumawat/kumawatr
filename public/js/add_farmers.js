5908ed102133b132eefd46978682073172601373
Rajpriya Kumawat <kumawatr@classwork.engr.oregonstate.edu>
2024-06-10 22:45:27 -0700

    Updating farmers files for collaborating

--- public/js/add_farmers.js
+++ public/js/add_farmers.js
@@ -1,3 +1,13 @@
+//<!-- 
+//Citation for general JavaScript files
+//Date: 06/10/2024
+//Adapted from JavaScript pages for the BSG Planets People Entity in the Developing in Node.JS Exploration Module.
+
+//Source URLs: 
+//- https://github.com/osu-cs340-ecampus/nodejs-starter-app
+//-->
+
+
 // Get the objects we need to modify
 let addFarmersForm = document.getElementById('add-farmer-form-ajax');
 
