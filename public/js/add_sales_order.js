716b742e578fae55ef25be75f622a748b25d248a
Rajpriya Kumawat <kumawatr@classwork.engr.oregonstate.edu>
2024-06-10 22:48:23 -0700

    Updating ales order file for collaborating

--- public/js/add_sales_order.js
+++ public/js/add_sales_order.js
@@ -1,3 +1,12 @@
+//<!-- 
+//Citation for general JavaScript files
+//Date: 06/10/2024
+//Adapted from JavaScript pages for the BSG Planets People Entity in the Developing in Node.JS Exploration Module.
+
+//Source URLs: 
+//- https://github.com/osu-cs340-ecampus/nodejs-starter-app
+//-->
+
 // Get the objects we need to modify
 let addSalesOrderForm = document.getElementById('add-sales-order-form-ajax');
 
