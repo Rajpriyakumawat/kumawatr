56d2462cad4059920e5c5aeccc04e88e6ea0681a
Sirus Salari <salaris@classwork.engr.oregonstate.edu>
2024-06-10 23:14:51 -0700

    Add citation to app.js

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
+//<!-- 
+//Citation for general JavaScript files
+//Date: 06/10/2024
+//Adapted from JavaScript pages for the BSG Planets People Entity in the Developing in Node.JS Exploration Module.
+
+//Source URLs: 
+//- https://github.com/osu-cs340-ecampus/nodejs-starter-app
+//-->
+
 /*
     SETUP
 */
